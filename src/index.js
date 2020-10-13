e2f6566d6ee3fca5632023d86ed13a79f459a36e
Lighnio <charlesvas16@outlook.com>
2020-10-13 16:32:50 -0600

    authentication ready, flash pending

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const engine = require('ejs-mate');
+const path = require('path');
+const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+// Initializations
+const app = express();
+require('./database');
+require('./passport/local-auth');
+
+// Settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extend:false}));
+app.use(session({
+    secret: 'teQuieroMuchoMiElotito',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash);
+
+// Routes
+app.use('/', require('./routes/index'))
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on Port: ', app.get('port'));
+});
---
+++ src/routes/index.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const router = express.Router();
+const passport = require('passport');
+
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    passReqToCallback: true
+}));
+
+router.get('/signin', (req, res, next) => {
+
+});
+
+
+router.post('/signin', (req, res, next) => {
+
+});
+
+router.get('/profile', (req, res, next) => {
+    res.render('profile');
+});
+
+
+
+module.exports = router;
